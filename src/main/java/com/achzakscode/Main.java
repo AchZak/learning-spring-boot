eead154ef600d30546d3c750a2794658fd2b31a8
AchZak <Achzak.programming@gmail.com>
2023-07-29 17:46:03 +0200

    Implemented CRUD operations for the Customer entity

--- src/main/java/com/achzakscode/Main.java
+++ src/main/java/com/achzakscode/Main.java
@@ -2,9 +2,7 @@ package com.achzakscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,25 @@ public class Main {
     @GetMapping
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        // We could also implement businesslogic
+        customerRepository.deleteById(id);
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age){
+
     }
 
 
